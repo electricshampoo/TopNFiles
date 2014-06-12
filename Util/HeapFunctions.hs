1db67263e0ca9381a2b8f823d538fba0dd36af30
Ganesh Rapolu <ganesh.rapolu@hotmail.com>
2014-06-11 15:37:21 -1000

    Refactored using ekmett's heap library.

--- Util/HeapFunctions.hs
+++ Util/HeapFunctions.hs
@@ -4,13 +4,13 @@ module Util.HeapFunctions
 
 import qualified Data.Heap as H
 
-boundedInsert :: Ord a => Int -> H.Heap H.MinPolicy a -> a -> H.Heap H.MinPolicy a
+boundedInsert :: Ord a => Int -> H.Heap a -> a -> H.Heap a
 boundedInsert 0 _ _ = H.empty
 boundedInsert k heap val
     | k < 0 = error "The bounding size should not be negative."
     | k < H.size heap = boundedInsert k heap' val
     | k == H.size heap = if val < minItem then heap else H.insert val heap'
     | otherwise = H.insert val heap
-    where (minItem, heap') = unsafeFromMaybe $ H.view heap
+    where (minItem, heap') = unsafeFromMaybe $ H.viewMin heap
           unsafeFromMaybe (Just x) = x 
           unsafeFromMaybe _ = error "This cannot happen."
--- topNFiles.hs
+++ topNFiles.hs
@@ -3,24 +3,18 @@ import Pipes.Prelude (fold)
 import System.Environment (getArgs)
 import System.Posix.Files (fileSize, getSymbolicLinkStatus)
 import System.Posix.Types (FileOffset)
-import Data.List (sortBy)
+import Data.Foldable (toList)
 import Util.StreamDirectory (getRecursiveContents)
 import Util.HeapFunctions (boundedInsert)
 import qualified Data.Heap as H
 
-data FilePathPair a = Pair FilePath a
-    deriving (Eq, Show)
-
-instance Ord a => Ord (FilePathPair a) where
-    compare (Pair _ x) (Pair _ y) = compare x y
-
-topFilesBy :: Ord a => (FilePath -> IO a) -> Int -> FilePath -> IO (H.Heap H.MinPolicy (FilePathPair a))
+topFilesBy :: Ord a => (FilePath -> IO a) -> Int -> FilePath -> IO (H.Heap (H.Entry a FilePath))
 topFilesBy f k = fold (boundedInsert k) H.empty id . getPairs where
     getPairs = getRecursiveContents ~> \file -> do
         val <- lift $ f file
-        yield $ Pair file val
+        yield $ H.Entry val file
 
-biggestFiles :: Int -> FilePath -> IO (H.Heap H.MinPolicy (FilePathPair FileOffset))
+biggestFiles :: Int -> FilePath -> IO (H.Heap (H.Entry FileOffset FilePath))
 biggestFiles = topFilesBy getFileSize where
     getFileSize = fmap fileSize . getSymbolicLinkStatus
 
@@ -28,4 +22,4 @@ main :: IO ()
 main = do
     [startDir, num] <- getArgs
     heap <- biggestFiles (read num) startDir
-    mapM_ print . sortBy (flip compare) . H.toList $ heap
+    mapM_ print . reverse . toList $ heap
